f2be89999c99eb45b5663c4aaa55670fc5d00133
Yongzhi Huang <yongzhihuang@yahoo.com>
2016-10-13 18:29:18 -0400

    Added placeholders and comments

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -2,12 +2,11 @@ chrome.extension.sendMessage({}, function(response) {
 	var readyStateCheckInterval = setInterval(function() {
 	if (document.readyState === "complete") {
 		clearInterval(readyStateCheckInterval);
+		// Placeholder player array for now, will figure out what to put later
+		var playerNames = [];
 
-		// ----------------------------------------------------------
-		// This part of the script triggers when page is done loading
-		console.log("Hello. This message was sent from scripts/inject.js");
-		// ----------------------------------------------------------
 
+		// Looks at page and replace every occurance of the known playerName with "teemo"
 	}
 	}, 10);
 });
